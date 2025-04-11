ca4b4275bab7e257ce79ce0cfbee50d93f69cd96
imanolortizfernandez <imanolof29@gmail.com>
2025-04-11 15:14:56 +0200

    fix: imports in modules

--- src/contexts/auth/infrastructure/auth.module.ts
+++ src/contexts/auth/infrastructure/auth.module.ts
@@ -2,14 +2,16 @@ import { Module } from "@nestjs/common";
 import { SignInController } from "./http/sign-in/sign-in.controller";
 import { SignInUseCase } from "../application/sign-in/sign-in";
 import { UsersModule } from "src/contexts/users/infrastructure/users.module";
-import { JwtModule } from "@nestjs/jwt";
-import { ConfigModule, ConfigService } from "@nestjs/config";
 import { SignUpController } from "./http/sign-up/sign-up.controller";
 import { SignUpUseCase } from "../application/sign-up/sign-up";
 import { VerifyUserUseCase } from "../application/verify/verify-user";
+import { VerifyUserController } from "./http/verify-user/verify-user.controller";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
     imports: [
+        UsersModule,
         JwtModule.registerAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
@@ -18,16 +20,16 @@ import { VerifyUserUseCase } from "../application/verify/verify-user";
                 signOptions: { expiresIn: '1d' },
             }),
         }),
-        UsersModule
     ],
     controllers: [
         SignInController,
-        SignUpController
+        SignUpController,
+        VerifyUserController,
     ],
     providers: [
         SignInUseCase,
         SignUpUseCase,
         VerifyUserUseCase
-    ]
+    ],
 })
 export class AuthModule { }
--- src/contexts/shared/infrastructure/shared.module.ts
+++ src/contexts/shared/infrastructure/shared.module.ts
@@ -1,14 +1,26 @@
 import { Module } from "@nestjs/common";
 import { EmailSenderService } from "../domain/services/email-sender.service";
 import { SendgridEmailSenderService } from "./email/sendgrid-email-sender.service";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "../auth/strategies/jwt.strategy";
+import { ConfigModule } from "@nestjs/config";
+import { UsersModule } from "src/contexts/users/infrastructure/users.module";
 
 @Module({
-    imports: [],
+    imports: [
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule,
+        ConfigModule,
+        UsersModule
+    ],
     providers: [
         {
             provide: EmailSenderService,
             useClass: SendgridEmailSenderService
-        }
-    ]
+        },
+        JwtStrategy
+    ],
+    exports: [PassportModule, JwtStrategy, JwtModule]
 })
 export class SharedModule { }
